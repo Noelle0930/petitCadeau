ac95f246220543e29646c320301b39c870d621a3
id <id@AQ-0010>
2023-06-22 14:54:12 +0900

    petitCadeau

---
+++ src/main/java/com/example/demo/PetitCadeauApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetitCadeauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetitCadeauApplication.class, args);
+	}
+
+}
