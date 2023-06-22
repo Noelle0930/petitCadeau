92ff68647f8193eac6a82a8d6badd4d318fdf1bf
id <id@AQ-0010>
2023-06-22 15:44:08 +0900

    6月22日15:43是村

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,3 +16,23 @@ CREATE TABLE items
    name TEXT,
    price INTEGER
 );
+
+-- 注文テーブル
+CREATE TABLE orders
+(
+  id SERIAL PRIMARY KEY,
+  event_id INTEGER,
+  ordered_on DATE,
+  send_address TEXT,
+  total_price INTEGER,
+  message TEXT
+);
+
+-- 注文詳細テーブル
+CREATE TABLE order_details
+(
+  id SERIAL PRIMARY KEY,
+  order_id INTEGER,
+  item_id INTEGER,
+  quantity INTEGER
+); 
