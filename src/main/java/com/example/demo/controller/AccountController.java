ca9a1468dbb6c35b337023ae696adb4201d91bd2
id <id@AQ-0124>
2023-06-26 09:50:37 +0900

    6/26 9:50 寺内 ログイン機能完成

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.Account;
+import com.example.demo.repository.UserRepository;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -12,8 +21,38 @@ public class AccountController {
 	@Autowired
 	HttpSession session;
 	
+	@Autowired
+	Account account;
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@GetMapping("/login")
 	public String index() {
 		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String login(
+			@RequestParam(name="email", required=false) String email,
+			@RequestParam(name="password", required=false) String password,
+			Model m) {
+		
+		Optional<User> login = userRepository.findByEmailAndPassword(email, password);
+		
+		String page;
+		String message = null;
+		
+		if (login.isEmpty()) {
+			message = "メールアドレスとパスワードが一致しませんでした";
+			page = "login";
+			m.addAttribute("message", message);
+		} else {
+			User user = login.get();
+			account.setName(user.getName());	
+			page = "redirect:/events";
+		}
+		
+		return page;
 	}
 }
