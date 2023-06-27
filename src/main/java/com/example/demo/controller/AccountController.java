511a529cb41c08930e3044ee7e7b7500bc01072f
id <id@AQ-0010>
2023-06-27 13:44:07 +0900

    0627 13:43 是村　会員登録エラーメッセージ完成

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -71,17 +73,57 @@ public class AccountController {
 
 	@PostMapping("/account/add")
 	public String storeUser(
-			@RequestParam(name = "name") String name,
-			@RequestParam(name = "address") String address,
-			@RequestParam(name = "email") String email,
-			@RequestParam(name = "tel") String tel,
-			@RequestParam(name = "password") String password,
-			@RequestParam(name = "birthday") LocalDate birthday,
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			@RequestParam(name = "birthday", required=false) LocalDate birthday,
 			Model model) {
 
+		Optional <User> record= userRepository.findByEmail(email);
+		
+		List<String> error=new ArrayList<>();
+		
+		if(name.equals("")) {
+			error.add("名前は必須です");	
+		}
+	 if(address.equals("")) {
+		 error.add("住所は必須です");
+		}
+	 if(tel.equals("")) {
+		 error.add("電話番号は必須です");
+		}
+	 if(email.equals("")){
+		 error.add("メールアドレスは必須です");	
+		}
+	 if(record.isEmpty()==false) {
+		 error.add("登録済みメールアドレスは登録できません");
+	    }
+	 if(password.equals("")){
+		 error.add("パスワードは必須です");	
+		}
+	 if(birthday==null) {
+		 error.add("誕生日は必須です");
+	 }
+	 
+	    model.addAttribute("name",name);
+		model.addAttribute("address",address);
+		model.addAttribute("tel",tel);
+		model.addAttribute("email",email);
+		model.addAttribute("password",password);
+		model.addAttribute("birthday",birthday);
+	    model.addAttribute("List",error);
+		
+	    if(error.size()==0) {
+	    	
+	    
 		User user = new User(name, address, email, tel, password, birthday);
 		userRepository.save(user);
 
 		return "redirect:/login";
-	}
+	    }
+	    
+	    return "/addUser";
+}
 }
