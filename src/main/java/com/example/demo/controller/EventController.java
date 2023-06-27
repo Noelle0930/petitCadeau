82bd24774f8b6ebdc15b02e16dc405b4b1f65f1b
id <id@AQ-0010>
2023-06-27 11:26:10 +0900

    0627 11:25 是村 イベント登録エラーメッセージ

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,15 +44,30 @@ public class EventController {
 
 	@PostMapping("/events/add")
 	public String eventStore(
-			@RequestParam(name = "name") String name,
-			@RequestParam(name = "eventDate") LocalDate eventDate,
+			@RequestParam(name = "name" ,defaultValue="") String name,
+			@RequestParam(name = "eventDate",required=false) LocalDate eventDate,
 			Model model) {
 
+		List<String> error=new ArrayList<>();
+		
+		if(name.equals("")) {
+			error.add("イベント名を入力してください");
+		}
+		if(eventDate==null) {
+			error.add("日付を指定してください");
+		}
+		
+		model.addAttribute("List",error);
+		
+		if(error.size()==0) {
 		Integer userId = account.getId();
 		
 		Event event = new Event(userId, name, eventDate);
 		eventRepository.save(event);
 		
 		return "redirect:/events";
+		}
+		
+		return "/addEvent";
 	}
 }
